9f113a3d1f2d1acb32acedeaf21149fd243ee02e
JohanJohansson <ras@ras-VirtualBox.(none)>
2014-12-01 14:23:59 +0100

    Adds camera frame

--- src/camera_tf_broadcaster.cpp
+++ src/camera_tf_broadcaster.cpp
@@ -4,21 +4,23 @@
 
 int main(int argc, char **argv) {
 
-    ros::init(argc, argv, "ir1_tf_broadcaster");
+    ros::init(argc, argv, "camera_tf_broadcaster");
 
     ros::NodeHandle n;
 
     static tf::TransformBroadcaster br;
     tf::Transform transform;
 
+    double angle = 14.5*M_PI/180;
+
     ros::Rate loop_rate(20.0);
     while (n.ok()) {
-        transform.setOrigin(tf::Vector3(-0.065, 0.06, 0.0));
+        transform.setOrigin(tf::Vector3(0.0, -0.05, -0.14));
         tf::Quaternion q;
-        q.setEuler(0.0, 0.0, M_PI_2);
+        q.setEuler(0.0, angle, 0.0);
         transform.setRotation(q);
 
-        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "robot_center", "sensor1"));
+        br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "robot_center", "camera"));
         loop_rate.sleep();
     }
 
